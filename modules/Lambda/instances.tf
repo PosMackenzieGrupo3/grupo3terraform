bbcf4a8c98304bbb1dc1d85730657c3223ba5ffc
fsouza-fs <felipe33737@gmail.com>
2022-12-03 15:28:49 -0300

    Fix errors

--- modules/Lambda/instances.tf
+++ modules/Lambda/instances.tf
@@ -11,6 +11,24 @@
                 "Service": "lambda.amazonaws.com"
             },
             "Effect": "Allow"
+        },
+        {
+            "Sid": "SpecificTable",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:BatchGet*",
+                "dynamodb:DescribeStream",
+                "dynamodb:DescribeTable",
+                "dynamodb:Get*",
+                "dynamodb:Query",
+                "dynamodb:Scan",
+                "dynamodb:BatchWrite*",
+                "dynamodb:CreateTable",
+                "dynamodb:Delete*",
+                "dynamodb:Update*",
+                "dynamodb:PutItem"
+            ],
+            "Resource": "arn:aws:dynamodb:*:*:table/webservicepos"
         }
     ]
 }
@@ -21,7 +39,7 @@ resource "aws_lambda_function" "lambda_inserir" {
   filename      = "inserir.zip"
   function_name = "lambda_inserir"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.handler"
+  handler       = "inserir.handler"
   runtime       = "python3.9"
 }
 
@@ -29,6 +47,6 @@ resource "aws_lambda_function" "lambda_consulta" {
   filename      = "consulta.zip"
   function_name = "lambda_consulta"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.handler"
+  handler       = "consuta.handler"
   runtime       = "python3.9"
 }
