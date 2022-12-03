ec69418cb325e86103a855cda36735447ff535f3
fsouza-fs <felipe33737@gmail.com>
2022-12-03 15:40:10 -0300

    Fix dynamo permission

--- modules/Lambda/instances.tf
+++ modules/Lambda/instances.tf
@@ -1,44 +1,19 @@
-  resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Effect": "Allow"
-        },
-        {
-            "Sid": "SpecificTable",
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:BatchGet*",
-                "dynamodb:DescribeStream",
-                "dynamodb:DescribeTable",
-                "dynamodb:Get*",
-                "dynamodb:Query",
-                "dynamodb:Scan",
-                "dynamodb:BatchWrite*",
-                "dynamodb:CreateTable",
-                "dynamodb:Delete*",
-                "dynamodb:Update*",
-                "dynamodb:PutItem"
-            ],
-            "Resource": "arn:aws:dynamodb:*:*:table/webservicepos"
-        }
-    ]
+resource "aws_iam_role" "role_for_LDC" {
+  name = "myrole"
+  assume_role_policy = file("assume_role_policy.json")
 }
-  EOF
+
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.role_for_LDC.id
+  policy = file("policy.json")
 }
 
 resource "aws_lambda_function" "lambda_inserir" {
   filename      = "inserir.zip"
   function_name = "lambda_inserir"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.role_for_LDC.arn
   handler       = "inserir.handler"
   runtime       = "python3.9"
 }
@@ -46,7 +21,7 @@ resource "aws_lambda_function" "lambda_inserir" {
 resource "aws_lambda_function" "lambda_consulta" {
   filename      = "consulta.zip"
   function_name = "lambda_consulta"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.role_for_LDC.arn
   handler       = "consuta.handler"
   runtime       = "python3.9"
 }
